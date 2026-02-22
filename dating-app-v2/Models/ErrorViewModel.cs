2961d4b0e3e279e4e312478792aa05bb1df42040
Chantal <chantal.zhang2018@gmail.com>
2026-02-22 16:24:09 -0500

    first commit

---
+++ dating-app-v2/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dating_app_v2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
